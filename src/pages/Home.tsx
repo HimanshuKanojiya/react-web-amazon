284f8ee6d5c0765ca826a13f0cbc8d2d35644692
Himanshu Kanojiya <himanshukanojiya825@gmail.com>
2022-03-14 23:38:24 +0530

    added global style but with wrong branch and will continue in new branch

---
+++ src/pages/Home.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Home: React.FC = () => {
+  return (
+    <>
+      <h1>This is home page</h1>
+      <h2>This is home page</h2>
+      <h3>This is home page</h3>
+      <h4>This is home page</h4>
+      <p>This is home page</p>
+    </>
+  );
+};
+
+export default Home;
