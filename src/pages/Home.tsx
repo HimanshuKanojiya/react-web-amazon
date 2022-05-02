f4b332682099e4983906c37901749fe1687a4e62
Himanshu Kanojiya <himanshukanojiya825@gmail.com>
2022-05-03 00:56:49 +0530

    added WIP notice for homepage

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,12 +3,28 @@ import React from "react";
 const Home: React.FC = () => {
   return (
     <>
-      <h1>This is home page</h1>
-      <h2>This is home page</h2>
-      <h3>This is home page</h3>
-      <h4>This is home page</h4>
-      <p>This is home page</p>
-      <span>This is home page</span>
+      <div>
+        <h1>WIP - Work in Progress</h1>
+        <p>
+          This project is in work in progress, please visit after few weeks to
+          see changes here...Thanks!
+        </p>
+        <span
+          style={{
+            display: "flex",
+            alignItems: "center",
+          }}
+        >
+          <strong
+            style={{
+              fontSize: "18px",
+            }}
+          >
+            Current development Status:
+          </strong>{" "}
+          <p>Sign Up page</p>
+        </span>
+      </div>
     </>
   );
 };
